e58135e95861937f8d710a6d3f4e85775daae802
Sathish reddy <104757035+sathish-nalladevagari@users.noreply.github.com>
2024-02-23 23:55:35 +0530

    updated download

--- components/intro.tsx
+++ components/intro.tsx
@@ -90,8 +90,8 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="../components/Sathish_CV.pdf"
-          download="Sathish_CV"
+          href="https://drive.google.com/file/d/19Kqt6kLXelZRchsL3lCMiPoTy6P8VyCk/view?usp=sharing"
+          download
         >
           Download CV{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
